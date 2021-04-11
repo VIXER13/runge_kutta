65688ef63fa9b4c5f9dcb9bc028d2a7d80f359f6
andrewSKLV <andrewsklv@oxygen>
2021-04-11 22:45:11 +0300

    .

--- include/runge_kutta.hpp
+++ include/runge_kutta.hpp
@@ -1,93 +1,63 @@
-#ifndef RUNGE_KUTTA_HPP
-#define RUNGE_KUTTA_HPP
+#ifndef RUNGE_KUTTA_RUNGE_KUTTA_HPP
+#define RUNGE_KUTTA_RUNGE_KUTTA_HPP
 
+#include "coefficients.hpp"
 #include "utils.hpp"
-#include <limits>
-#include <tuple>
 #include <vector>
+#include <iostream>
+
+namespace ode {
 
 template<class T>
-class runge_kutta4 {
-    static_assert(std::is_floating_point<T>{}, "T must be floating point.");
-
-    using p = std::pair<size_t, T>;
-    template<size_t N>
-    using a = std::array<p, N>;
-
-public:
-    static constexpr auto aij = std::make_tuple(
-        a<1>{p{0,        T{0}}},
-        a<1>{p{0, T{1} / T{2}}},
-        a<1>{p{1, T{1} / T{2}}},
-        a<1>{p{2,        T{1}}}
-    );
-
-    static constexpr size_t stages = std::tuple_size<decltype(aij)>{};
-
-    static constexpr std::array<T, stages> ci = {T{0}, T{1} / T{2}, T{1} / T{2}, T{1}};
-    static constexpr std::array<T, stages> bj = {T{1} / T{6}, T{1} / T{3}, T{1} / T{3}, T{1} / T{6}};
-};
-
-template<class T>
-struct parameters {
-    T tau_min  = 0,
-      tau_init = T{1} / T{10},
-      tau_max  = std::numeric_limits<T>::max(),
-      tol      = T{1} / T{1000},
-      fact_min = T{1} / T{2},
-      fact     = T{1},
-      fact_max = T{2};
-    std::array<T, 2> time_interval = {T{0}, T{1}};
-};
-
-class _runge_kutta {
-    template<size_t I, class T, template<class> class Coeffs, class Container, class System>
-    static void stage(std::array<Container, Coeffs<T>::stages>& k,
-                      const Container& sol, const T time, const T tau, const System& system) {
-        std::fill(k[I].begin(), k[I].end(), T{0});
-        using namespace utils;
-        for(const auto [ind, val] : std::get<I>(Coeffs<T>::aij))
-            k[I] += val * k[ind];
-        k[I] = system(time + tau * Coeffs<T>::ci[I], sol + tau * k[I]);
-    }
-
-    template<class T, template<class> class Coeffs, class Container, class System, size_t ...I>
-    static void make_step_impl(std::array<Container, Coeffs<T>::stages>& k,
-                               const Container& sol, const T time, const T tau, const System& system, std::index_sequence<I...>) {
-        (stage<I, T, Coeffs>(k, sol, time, tau, system), ...);
-    }
-
-    template<class T, template<class> class Coeffs, class Container, class System>
-    static void make_step(std::array<Container, Coeffs<T>::stages>& k,
-                          const Container& sol, const T time, const T tau, const System& system) {
-        make_step_impl<T, Coeffs>(k, sol, time, tau, system, std::make_index_sequence<Coeffs<T>::stages>{});
-    }
-
-public:
-    template<class T, template<class> class Coeffs, class Container, class System>
-    friend std::pair<std::vector<T>, std::vector<Container>>
-    runge_kutta(const parameters<T>& params, const Container& init, const System& system);
+struct runge_kutta_parameters {
+    T tau_min  = 0,                                // Минимальный размер шага
+    tau_init = T{1} / T{10},                     // Начальный размер шага
+    tau_max  = std::numeric_limits<T>::max(),    // Максимальный размер шага
+    tol      = T{1} / T{1000},                   // Точность
+    fact_min = T{1} / T{2},                      // Минимальный фактор
+    fact     = T{1},                             // Гарантийный фактор
+    fact_max = T{2};                             // Максимальный фактор
+    std::array<T, 2> time_interval = {T{0}, T{1}}; // Интервал интегрирования
 };
 
 template<class T, template<class> class Coeffs, class Container, class System>
 std::pair<std::vector<T>, std::vector<Container>>
-runge_kutta(const parameters<T>& params, const Container& init, const System& system) {
-    Container K = init;
-    std::vector<T> time(1, params.time_interval.front());
+runge_kutta(const runge_kutta_parameters<T>& parameters, const Container& init, const System& system) {
     std::vector<Container> sol(1, init);
-    std::array<Container, Coeffs<T>::stages> k;
-    k.fill(init);
-    const uintmax_t steps = (params.time_interval.back() - params.time_interval.front()) / params.tau_init;
-    for(uintmax_t i = 0; i < steps; ++i) {
-        _runge_kutta::template make_step<T, Coeffs>(k, sol.back(), time.back(), params.tau_init, system);
-        std::fill(K.begin(), K.end(), T{0});
+    std::vector<T> time(1, parameters.time_interval.front());
+    std::array<Container, Coeffs<T>::stages> k; k.fill(init);
+    std::array<Container, Coeffs<T>::nested ? 2 : 1> K; K.fill(init);
+    const T tau = parameters.tau_init;
+    while (time.back() < parameters.time_interval.back()) {
         using namespace utils;
-        for(size_t i = 0; i < Coeffs<T>::stages; ++i)
-            K += Coeffs<T>::bj[i] * k[i];
-        time.push_back(time.back() + params.tau_init);
-        sol.push_back(sol.back() + params.tau_init * K);
+        size_t curr = 0;
+        for(const auto& [i, j, val] : Coeffs<T>::aij) {
+            if (i == curr) {
+                k[i] += val * k[j];
+            } else {
+                k[curr] = system(time.back() + tau * Coeffs<T>::ci[curr], sol.back() + tau * k[curr]);
+                k[i] = val * k[j];
+                curr = i;
+            }
+        }
+        k.back() = system(time.back() + tau * Coeffs<T>::ci[curr], sol.back() + tau * k.back());
+
+        curr = -1;
+        for(const auto& [i, j, val] : Coeffs<T>::bj) {
+            if (i == curr)
+                K[i] += val * k[j];
+            else {
+                K[i] = val * k[j];
+                curr = i;
+            }
+        }
+
+        time.push_back(time.back() + parameters.tau_init);
+        sol.push_back(sol.back() + parameters.tau_init * K.front());
     }
     return {std::move(time), std::move(sol)};
+}
+
 }
 
 #endif
--- main.cpp
+++ main.cpp
@@ -26,33 +26,37 @@ int main() {
     static constexpr uintmax_t Nvar = 5;
     static constexpr double t0 = 0.1 * Nvar;
 
-    parameters<double> params;
-    params.tau_init = 0.000001;
-    params.time_interval = {t0, t0 + 4};
-    const std::array<double, 2> init = {std::exp(std::sin(t0 * t0)), std::exp(std::cos(t0 * t0))};
+    ode::runge_kutta_parameters<double> parameters;
+    parameters.tau_init = 0.1;
+    //parameters.time_interval = {t0, t0 + 4};
+    //const std::array<double, 2> init = {std::exp(std::sin(t0 * t0)), std::exp(std::cos(t0 * t0))};
+    parameters.time_interval = {0, 10};
+    const std::array<double, 2> init = {1, 0};
     static constexpr auto system = [](const double t, const std::array<double, 2>& x) {
         return std::array<double, 2>{
-             2 * t * x[0] * std::cos(t * t),
-            -2 * t * x[1] * std::sin(t * t)
+             //2 * t * x[0] * std::cos(t * t),
+            //-2 * t * x[1] * std::sin(t * t)
+            -std::sin(t),
+            -std::cos(t)
         };
     };
 
-    auto [time, sol] = runge_kutta<double, runge_kutta4>(params, init, system);
+    auto [time, sol] = ode::runge_kutta<double, ode::dormand_prince>(parameters, init, system);
     std::cout << "steps = " << time.size()-1 << std::endl;
-    save_data("test", time, sol, 1000);
+    save_data("test", time, sol, 1);
 
-//    params.tau_init /= 2;
-//    auto [time2, sol2] = runge_kutta<double, runge_kutta4>(params, std::array<double, 2>{1, 0}, system);
-//    std::cout << "steps = " << time2.size()-1 << std::endl;
-//
-//    double err1 = 0, err2 = 0;
-//    for(size_t i = 0; i < time.size()-1; ++i) {
-//        err1 += std::abs(sol[i][0] - std::cos(time[i]));
-//        err2 += std::abs(sol2[2*i][0] - std::cos(time[i]));
-//    }
-//    std::cout << "err1 = " << err1 << std::endl;
-//    std::cout << "err2 = " << err2 << std::endl;
-//    std::cout << "err1 / err2 = " << err1 / err2 << std::endl;
+    parameters.tau_init /= 2;
+    auto [time2, sol2] = ode::runge_kutta<double, ode::dormand_prince>(parameters, init, system);
+    std::cout << "steps = " << time2.size()-1 << std::endl;
+
+    double err1 = 0, err2 = 0;
+    for(size_t i = 0; i < time.size()-1; ++i) {
+        err1 += std::abs(sol [  i][0] - std::cos(time[i]));
+        err2 += std::abs(sol2[2*i][0] - std::cos(time[i]));
+    }
+    std::cout << "err1 = " << err1 << std::endl;
+    std::cout << "err2 = " << err2 << std::endl;
+    std::cout << "err1 / err2 = " << err1 / err2 << std::endl;
 
     return 0;
 }
