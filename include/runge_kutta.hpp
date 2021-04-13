5fcf3c0109edbc73a9a51d99c9e73708b837eafa
andrewSKLV <andrewsklv@oxygen>
2021-04-13 20:06:52 +0300

    Мелкие правки

--- include/runge_kutta.hpp
+++ include/runge_kutta.hpp
@@ -20,9 +20,12 @@ struct runge_kutta_parameters {
       fact      = T{1},                            // Гарантийный фактор
       fact_max  = T{2};                            // Максимальный фактор
     std::array<T, 2> time_interval = {T{0}, T{1}}; // Интервал интегрирования
+    bool autoresize = true;
 };
 
 class _runge_kutta {
+    enum class STEP_RESIZE_FLAG : uint8_t {FIX, RESIZE, NO};
+
     template<class T, template<class> class Coeffs, class Container, class System>
     static void make_step(std::array<Container, Coeffs<T>::stages>& k,
                           const T time, const Container& sol, const T tau, const System& system) {
@@ -59,12 +62,21 @@ class _runge_kutta {
     static T calc_error(std::array<Container, 2>& K) {
         T err = 0;
         for(size_t i = 0; i < K[0].size(); ++i) {
-            const T temp = Nested ? std::abs((K[0][i] - K[1][i]) / K[1][i]) : std::abs(K[0][i] - K[1][i]);
+            const T temp = std::abs(Nested ? (K[0][i] - K[1][i]) / std::max(K[0][i], K[1][i]) :
+                                              K[0][i] - K[1][i]);
             if(temp > err)
                 err = temp;
         }
         static constexpr T factor = T{1} / (utils::power<Order>(2) - 1);
         return factor * err;
+    }
+
+    template<class T, class Container>
+    static void calc_new_sol(std::array<Container, 2>& new_sol, const Container& sol,
+                             const std::array<Container, 2>& K, const T tau) {
+        using namespace utils;
+        for(size_t i = 0; i < 2; ++i)
+            new_sol[i] = sol + tau * K[i];
     }
 
     template<uintmax_t Order, class T>
@@ -75,39 +87,54 @@ class _runge_kutta {
                         parameters.fact * std::pow(parameters.tol / err, power)));
     }
 
+    template<class T, template<class> class Coeffs, class Container, class System>
+    static STEP_RESIZE_FLAG resize_tau(T& tau, std::array<Container, 2>& new_sol,
+                                       const runge_kutta_parameters<T>& parameters,
+                                       const T time, const Container& x,
+                                       const std::array<Container, 2>& K,
+                                       const std::array<Container, Coeffs<T>::stages>& k) {
+
+    }
+
 public:
     template<class T, template<class> class Coeffs, class Container, class System>
-    friend std::pair<std::vector<T>, std::vector<Container>>
+    friend std::tuple<std::vector<T>, std::vector<Container>, std::vector<T>>
     runge_kutta(const runge_kutta_parameters<T>& parameters, const Container& init, const System& system);
 };
 
 template<class T, template<class> class Coeffs, class Container, class System>
-std::pair<std::vector<T>, std::vector<Container>>
+std::tuple<std::vector<T>, std::vector<Container>, std::vector<T>>
 runge_kutta(const runge_kutta_parameters<T>& parameters, const Container& init, const System& system) {
     T tau = parameters.tau_init;
     std::vector<Container> sol(1, init);
     std::vector<T> time(1, parameters.time_interval.front());
     std::array<Container, Coeffs<T>::stages> k; k.fill(init);
     std::array<Container, 2> K; K.fill(init);
+    std::array<Container, 2> new_sol;
+    std::vector<T> loc_err(1, 0);
     while (time.back() < parameters.time_interval.back()) {
-        bool step_resize = true;
+        bool step_resize = parameters.autoresize;
         while (true) {
             _runge_kutta::template make_step<T, Coeffs>(k, time.back(), sol.back(), tau, system);
             _runge_kutta::template calc_sol_step<T, Coeffs, 0>(K, k);
 
-            if (!step_resize)
+            if (!step_resize) {
+                _runge_kutta::template calc_new_sol(new_sol, sol.back(), K, tau);
                 break;
+            }
 
             if constexpr (Coeffs<T>::nested) {
-                const T err = _runge_kutta::template calc_error<Coeffs<T>::order, Coeffs<T>::nested, T>(K);
+                _runge_kutta::template calc_new_sol(new_sol, sol.back(), K, tau);
+                const T err = _runge_kutta::template calc_error<Coeffs<T>::order, Coeffs<T>::nested, T>(new_sol);
                 if (err < parameters.tol)
                     break;
                 tau *= _runge_kutta::template tau_factor<Coeffs<T>::order>(parameters, err);
             } else {
                 _runge_kutta::template make_step<T, Coeffs>(k, time.back(), sol.back(), 0.5 * tau, system);
                 _runge_kutta::template calc_sol_step<T, Coeffs, 1>(K, k);
-                const T err = _runge_kutta::template calc_error<Coeffs<T>::order, Coeffs<T>::nested, T>(K);
-                if (err < parameters.tol_min)
+                _runge_kutta::template calc_new_sol(new_sol, sol.back(), K, tau);
+                const T err = _runge_kutta::template calc_error<Coeffs<T>::order, Coeffs<T>::nested, T>(new_sol);
+                if (err < parameters.tol_small)
                     tau *= parameters.fact_max;
                 else if (err < parameters.tol)
                     break;
@@ -127,11 +154,11 @@ runge_kutta(const runge_kutta_parameters<T>& parameters, const Container& init, 
                 step_resize = false;
             }
         }
-        using namespace utils;
+        loc_err.push_back(_runge_kutta::template calc_error<Coeffs<T>::order, Coeffs<T>::nested, T>(new_sol));
         time.push_back(time.back() + tau);
-        sol.emplace_back(sol.back() + tau * K[0]);
+        sol.emplace_back(std::move(new_sol[0]));
     }
-    return {std::move(time), std::move(sol)};
+    return {std::move(time), std::move(sol), std::move(loc_err)};
 }
 
 }
--- main.cpp
+++ main.cpp
@@ -2,21 +2,34 @@
 #include <string_view>
 #include <iostream>
 #include <fstream>
+#include <omp.h>
 
 namespace {
 
 template<class T, size_t N>
-void save_data(const std::string& path, const std::vector<T>& time, const std::vector<std::array<T, N>>&sol,
+void save_data(const std::string& path,
+               const std::vector<T>& time,
+               const std::vector<std::array<T, N>>& sol,
+               const std::vector<T>& loc_err,
                const size_t save_freq = 1) {
     std::ofstream fout;
     for(size_t i = 0; i < N; ++i) {
         fout.open(path + std::to_string(i) + ".csv");
-        for(size_t j = 0; j < time.size(); ++j) {
+        for(size_t j = 0; j < time.size(); ++j)
             if (j % save_freq == 0)
                 fout << time[j] << ',' << sol[j][i] << '\n';
-        }
         fout.close();
     }
+    fout.open(path + "_loc_err" + ".csv");
+    for(size_t j = 0; j < time.size(); ++j)
+        if (j % save_freq == 0)
+            fout << time[j] << ',' << loc_err[j] << '\n';
+    fout.close();
+    fout.open(path + "_step" + ".csv");
+    for(size_t j = 0; j < time.size()-1; ++j)
+        if (j % save_freq == 0)
+            fout << time[j] << ',' << time[j+1] - time[j] << '\n';
+    fout.close();
 }
 
 }
@@ -24,22 +37,24 @@ void save_data(const std::string& path, const std::vector<T>& time, const std::v
 int main() {
     static constexpr uintmax_t Nvar = 5;
     static constexpr double t0 = 0.1 * Nvar;
-
-    ode::runge_kutta_parameters<double> parameters;
-    parameters.tau_init = 0.1;
-    parameters.tol = 1e-3;
-    parameters.time_interval = {t0, t0 + 4};
-    const std::array<double, 2> init = {std::exp(std::sin(t0 * t0)), std::exp(std::cos(t0 * t0))};
+    static constexpr std::array<double, 2> init = {std::exp(std::sin(t0 * t0)),
+                                                   std::exp(std::cos(t0 * t0))};
     static constexpr auto system = [](const double t, const std::array<double, 2>& x) {
         return std::array<double, 2>{
-             2 * t * x[0] * std::cos(t * t),
-            -2 * t * x[1] * std::sin(t * t)
+             2 * t * x[0] * std::log(std::max(x[1], 0.001)),
+            -2 * t * x[1] * std::log(std::max(x[0], 0.001))
         };
     };
-
-    auto [time, sol] = ode::runge_kutta<double, ode::dormand_prince>(parameters, init, system);
+    ode::runge_kutta_parameters<double> parameters;
+    parameters.time_interval = {t0, t0 + 4};
+    parameters.tau_init = 0.1;
+    parameters.tol = 1e-6;
+    parameters.fact = 1;
+    parameters.tol_small = parameters.tol * parameters.tol;
+    double calc_time = omp_get_wtime();
+    auto [time, sol, loc_err] = ode::runge_kutta<double, ode::runge_kutta4>(parameters, init, system);
+    std::cout << "time = " << omp_get_wtime() - calc_time << std::endl;
     std::cout << "steps = " << time.size()-1 << std::endl;
-    save_data("test", time, sol, 1);
-
-    return 0;
+    //save_data("eps3dp", time, sol, loc_err, 1);
+    return EXIT_SUCCESS;
 }
